4e5065e5dfebb396fe54d8595465f700c323bc70
Ryan Nielsen <ryan@tumultco.com>
2011-07-16 00:01:28 -0700

    Initial Commit

---
+++ Shadeoclock/ShadeoclockView.h
@@ -0,0 +1,13 @@
+//
+//  ShadeoclockView.h
+//  Shadeoclock
+//
+//  Created by Ryan Nielsen on 7/16/11.
+//  Copyright 2011 Tumult Inc. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface ShadeoclockView : ScreenSaverView
+
+@end
---
+++ Shadeoclock/ShadeoclockView.m
@@ -0,0 +1,52 @@
+//
+//  ShadeoclockView.m
+//  Shadeoclock
+//
+//  Created by Ryan Nielsen on 7/16/11.
+//  Copyright 2011 Tumult Inc. All rights reserved.
+//
+
+#import "ShadeoclockView.h"
+
+@implementation ShadeoclockView
+
+- (id)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
